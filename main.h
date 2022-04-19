f2030035707dc75ea2f6f8568dd6b7f197996b3b
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-04-19 12:00:56 +0300

    updated main

--- main.h
+++ main.h
@@ -21,7 +21,7 @@ typedef struct convert conver_t;
 
 int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
-int _write_char(char);
+int _putchar(char);
 int print_char(va_list);
 int print_string(va_list);
 int print_percent(va_list);
