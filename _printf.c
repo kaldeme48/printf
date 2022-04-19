cd2140aaee1331d3026265a85a224be6b56fd564
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-04-19 09:53:50 +0300

    printf updated

--- _printf.c
+++ _printf.c
@@ -1,81 +1,93 @@
 #include "main.h"
+
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+ * _printf - Entry point
+ *
+ * @format: A string containing all the desired characters
+ *
+ * Description: Receives the main string and all the necessary parameters to
+ * print a formated string
+ *
+ * Return: A total count of the characters printed
  */
-int _printf(const char * format, ...)
-{   
-    const char *traverse;
-    int length = 0;
-	va_list args;
-	va_start(args, format);
-	
-	if ( format == NULL )
+
+int _printf(const char *format, ...)
+{
+	int printed_chars;
+	con_vert f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{NULL, NULL}
+	};
+	va_list arg_list;
+
+	if (format == NULL)
 		return (-1);
 
-    for(traverse = format; *traverse != '\0'; traverse++) 
-	{ 
-        while( *traverse != '%' )
-		{   
-		    _putchar(*traverse);
-                     traverse++; 
-                     length++;     	
-		}
-    
-        traverse++;
-        length++; 
+	va_start(arg_list, format);
 
-       
-        switch(*traverse) 
-        { 
-            case 'c' : printf_char;        
-                        break; 
-                        
-            case 'i' : printf_int;        
-                        break; 
-						          
-            case 'u' : printf_unsigned;        
-                        break; 
-
-            case 'd' : printf_dec;        
-                        break; 
-
-            case 'o' : printf_oct;
-                        break; 
-
-            case 's':  printf_string;                              
-                        break; 
-
-            case 'p': printf_pointer;
-                        break;
-                        
-	    case '%': printf_37;
-			break;
-                        
-	    case 'x': printf_hex;                         
-                        break;
-             
-	    case 'X': printf_HEX;    
-                        break;            
-                        
-           case 'b': printf_bin;    
-                        break; 
-                        
-            case 'S': printf_exclusive_string;
-                        break; 
-                        
-            case 'r': printf_srev;
-                        break; 
-                        
-            case 'R': printf_rot13;
-                        break; 
-                        
-                        
-        }   
-    
-	}
-      	
-	va_end(args);
-	return (length);
+	/*Calling parser function*/
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
 }
+
+/**
+ * parser - Entry Point
+ *
+ * Description: Receives the main string and all the necessary parameters to
+ * print a formated string.
+ *
+ * @format: A string containing all the desired characters.
+ * @f_list: A list of all the posible functions.
+ * @arg_list: A list containing all the argumentents passed to the program.
+ *
+ * Return: A total count of the characters printed.
+ */
+
+int parser(const char *format, con_vert f_list[], va_list arg_list)
+{
+	int i, j, r_val, printed_chars;
+
+	printed_chars = 0;
+	for (i = 0; format[i] != '\0'; i++) /* Iterates through the main str*/
+	{
+		if (format[i] == '%') /*Checks for format specifiers*/
+		{
+			/*Iterates through struct to find the right func*/
+			for (j = 0; f_list[j].sym != NULL; j++)
+			{
+				if (format[i + 1] == f_list[j].sym[0])
+				{
+					r_val = f_list[j].f(arg_list);
+					if (r_val == -1)
+						return (-1);
+					printed_chars += r_val;
+					break;
+				}
+			}
+			if (f_list[j].sym == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != '\0')
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					printed_chars = printed_chars + 2;
+				}
+				else
+					return (-1);
+			}
+			i = i + 1; /*Updating i to skip format symbols*/
+		}
+		else
+		{
+			_putchar(format[i]); /*call the write function*/
+			printed_chars++;
+		}
+	}
+	return (printed_chars);
+}
+
